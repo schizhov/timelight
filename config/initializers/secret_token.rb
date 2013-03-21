345b94b40cf39e23b549a830da2e617891ec1f33
Stas Chizhov <schizhov@gmail.com>
2013-03-21 19:16:29 +0100

    Initial commit. Only sqlite moved to development group in addition to results of rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timelight::Application.config.secret_token = '42459667ccf1d4e8de061602dfe747659bad166c4d3f37985898235828feff5799130c4b785931ad70b9a2fd8c4b77dbeab76ee49d6c945b24f19376157ebccd'
